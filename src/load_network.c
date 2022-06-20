dafe472a099307cab90c8adeaaaaab917660c940
Bora Bulut <bora@dynamic-10-125-216-216.frisco.wireless.unt.edu>
2022-06-20 13:26:37 -0500

    Modified load_network.c

--- src/load_network.c
+++ src/load_network.c
@@ -27,49 +27,77 @@ float read_float(FILE* file, unsigned char* buffer) {
 void load_network() {
 	int length = sizeof(unsigned char) * 4;
 	unsigned char *buffer = malloc(length);
+    int *magic_number_ptr = malloc(sizeof(int));
+    int *num_layers_ptr = malloc(sizeof(int));
+    int *len_of_weights_shape_ptr;
+    int* weights_shape_ptr;
+    int *shape_value_ptr;
+    float *data_ptr;
 	FILE *file = fopen("/Users/bora/Desktop/VGG-Simulation/data/network.nn", "rb");
 	if (file == NULL) {
 		fprintf(stderr, "Error opening file!\n");
 	}
 	
-	int magic_number = read_int(file, buffer); 
-    //printf("Magic number: %d \n", magic_number);
+	int magic_number = read_int(file, buffer);
+    *magic_number_ptr = magic_number;
+    printf("Magic number: %d \n", *magic_number_ptr);
 	int number_of_layers = read_int(file, buffer);
-    //printf("Number of layers: %d \n", number_of_layers);
+    *num_layers_ptr = number_of_layers;
+    printf("Number of layers: %d \n", *num_layers_ptr);
+    int *layer_type_ptr = malloc(sizeof(int) * (*num_layers_ptr));
+    int *num_weight_indices_ptr = malloc(sizeof(int) * (*num_layers_ptr));
+    int *activation_type_ptr = malloc(sizeof(int) * (*num_layers_ptr));
 	for (int layer_index = 0; layer_index < number_of_layers; layer_index++) {
 		int layer_type = read_int(file, buffer);
-        //printf("Layer index: %d \n", layer_index);
-        //printf("Layer type: %d \n", layer_type);
+        layer_type_ptr[layer_index] = layer_type;
+        printf("Layer index: %d \n", layer_index);
+        printf("Layer type: %d \n", layer_type_ptr[layer_index]);
         if (layer_type == 1) {
             int type_of_activation = read_int(file, buffer);
-            //printf("Type of activation: %d \n", type_of_activation);
+            activation_type_ptr[layer_index] = type_of_activation;
+            printf("Type of activation: %d \n", activation_type_ptr[layer_index]);
         }
         else if (layer_type == 4) {
             int type_of_activation = read_int(file, buffer);
-            //printf("Type of activation: %d \n", type_of_activation);
+            activation_type_ptr[layer_index] = type_of_activation;
+            printf("Type of activation: %d \n", activation_type_ptr[layer_index]);
         }
         int number_of_weight_indices = read_int(file, buffer);
-        //printf("Number of weight indices: %d \n", number_of_weight_indices);
+        num_weight_indices_ptr[layer_index] = number_of_weight_indices;
+        printf("Number of weight indices: %d \n", (num_weight_indices_ptr[layer_index]));
+        len_of_weights_shape_ptr = malloc(sizeof(int) * number_of_weight_indices);
         for (int weight_index = 0; weight_index < number_of_weight_indices; weight_index++) {
             int length_of_weights_shape = read_int(file, buffer);
-            //printf("Length of weights shape: %d \n", length_of_weights_shape);
-            int* weights_shape = (int*) malloc(length_of_weights_shape*sizeof(int));
+            len_of_weights_shape_ptr[weight_index] = length_of_weights_shape;
+            printf("Length of weights shape: %d \n", len_of_weights_shape_ptr[weight_index]);
+            weights_shape_ptr = (int*) malloc(length_of_weights_shape*sizeof(int));
             for (int shape_index = 0; shape_index < length_of_weights_shape; shape_index++) {
                 int shape_value = read_int(file, buffer);
-                //printf("Shape value: %d \n", shape_value);
-                weights_shape[shape_index] = shape_value;
+                weights_shape_ptr[shape_index] = shape_value;
+                printf("Shape value: %d \n", weights_shape_ptr[shape_index]);
             }
             int number_of_data_stored_in_shape = 1;
             for (int i = 0; i < length_of_weights_shape; i++) {
-                number_of_data_stored_in_shape *= weights_shape[i];
+                number_of_data_stored_in_shape *= weights_shape_ptr[i];
             }
+            data_ptr = (float*) malloc(number_of_data_stored_in_shape*sizeof(float));
             for (int i = 0; i < number_of_data_stored_in_shape; i++) {
                 float ba = read_float(file, buffer);
-                //printf("Data: %f \n", ba);
+                data_ptr[i] = ba;
+                printf("Data: %f \n", data_ptr[i]);
             }
         }
 	}
-	free(buffer); 
+	free(buffer);
+    free(magic_number_ptr);
+    free(num_layers_ptr);
+    free(layer_type_ptr);
+    free(num_weight_indices_ptr);
+    free(len_of_weights_shape_ptr);
+    free(weights_shape_ptr);
+    free(data_ptr);
+    free(activation_type_ptr);
+    fclose(file);
 }
 
 /*
