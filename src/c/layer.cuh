47d4e431c3e90cd7145d6f7ef3bcafc8059d75c2
justinmgarrigus <justin.m.garrigus@gmail.com>
2022-07-07 13:59:29 -0500

    Replaced activation pointer with enum

--- src/c/layer.cuh
+++ src/c/layer.cuh
@@ -26,7 +26,7 @@ struct layer {
 	int weight_set_count;
 	ndarray **weights;
 	void (*feed)(layer* input, layer* op);
-	ND_TYPE (*activation)(ND_TYPE* value);
+	enum layer_activation activation; 
 	ndarray *outputs;
 }; 
 
@@ -38,6 +38,7 @@ void layer_max_pooling_feedforward(layer* input_layer, layer* pool_layer);
 void layer_flatten_feedforward(layer* input_layer, layer* flatten_layer); 
 void layer_dense_feedforward(layer* input_layer, layer* dense_layer); 
 
+__device__ ND_TYPE layer_activation(ND_TYPE* value); 
 __device__ ND_TYPE layer_relu(ND_TYPE* value); 
 __device__ ND_TYPE layer_softmax(ND_TYPE* value); 
 
